f1896e354fd30f6e94c519c883ca27e778bd0ad6
skrjsghd <alsrjsghd1@gmail.com>
2024-04-10 21:47:43 +0900

    FEAT : init color theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,8 @@
 import type { Config } from "tailwindcss";
+import Colors from "tailwindcss/colors";
 
 const config: Config = {
+  darkMode: ["selector"],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,7 +10,27 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      container: {},
+      colors: {
+        p: Colors.blue,
+        m: Colors.stone,
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        primary: {
+          foreground: "var(--primary-foreground)",
+          DEFAULT: "var(--primary)",
+        },
+        muted: {
+          foreground: "var(--muted-foreground)",
+          DEFAULT: "var(--muted)",
+        },
+        border: "var(--border)",
+        ring: "var(--ring)",
+      },
+    },
+    borderRadius: {
+      lg: `var(--radius)`,
+      md: `calc(var(--radius) - 2px)`,
+      sm: "calc(var(--radius) - 4px)",
     },
   },
   plugins: [],
